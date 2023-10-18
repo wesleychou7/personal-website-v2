99b2452dcfce386b9999bddd36bb424ff657ac22
Wesley Chou <wesleychou9851@gmail.com>
2023-10-17 21:32:46 -0700

    progress

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,23 @@
 function displayEmail() {
 
-    element = document.getElementById("email-box");
+    let element = document.getElementById("email-box");
 
     if (element.style.visibility == "hidden" || element.style.visibility == ""){
         element.style.visibility = "visible";
     } else {
         element.style.visibility = "hidden";
     }
+}
+
+function copyEmail() {
+    var email = document.getElementById("email").innerHTML;
+    navigator.clipboard.writeText(email);
+
+    let element = document.getElementById("copy");
+
+    // change element's image source for 2 seconds, and switch back to original
+    element.src = "images/check.png";
+    setTimeout(function() {
+        element.src = "images/copy.png";
+    }, 1500);
 }
